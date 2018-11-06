c91b3b24bddfd766588fc6a8bfffc3305b189193
Matthieu Di Mercurio <matthieu.dimercurio@gmail.com>
2018-11-06 17:53:02 -0500

    monthly NPS (#22)

---
+++ models/delighted/transform/delighted_nps_patient_monthly.sql
@@ -0,0 +1,16 @@
+with
+	nps_survey as (
+		select * from {{ ref('delighted_nps_patient_survey') }}
+	)
+
+select date_trunc('month', received_at) as date_month
+  , round(100.0 * (count(*) filter(where category = 'promoter')
+      - count(*) filter (where category = 'detractor')
+    ) / count(*)) as nps
+  , count(*) filter(where category = 'promoter') as promoter_count
+  , count(*) filter(where category = 'passive') as passive_count
+  , count(*) filter(where category = 'detractor') as detractor_count
+  , count(*) as respondent_count
+from nps_survey
+where date_trunc('month', received_at) < date_trunc('month', current_date)
+group by 1
