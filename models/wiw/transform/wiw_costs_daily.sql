9630558efe230441cce4a3d76a8fc61270e37fcd
Jacob <38863038+jacobfrackson@users.noreply.github.com>
2018-11-13 10:42:30 -0500

    Feat: wiw costs by location (#37)
    * WIW Costs Daily
    
    * Add test
    
    * Remove additional day on account of decree

---
+++ models/wiw/transform/wiw_costs_daily.sql
@@ -0,0 +1,30 @@
+with shifts as (
+	select * from {{ ref( 'wiw_shifts' ) }}
+)
+
+select start_date_est as start_date
+	, position_name
+	, sum(cost) as cost
+	, sum(hours) as hours_worked
+	, sum(cost) 
+		filter(where location_name = 'Virtual Care Platform')
+			as virtual_costs
+	, sum(hours) 
+		filter(where location_name = 'Virtual Care Platform')
+			as virtual_hours
+	, sum(cost) 
+		filter(where location_name = 'Admin')
+			as admin_costs
+	, sum(hours) 
+		filter(where location_name = 'Admin')
+			as admin_hours
+	, sum(cost) 
+		filter(where location_name = 'Ubisoft')
+			as ubisoft_costs
+	, sum(hours) 
+		filter(where location_name = 'Ubisoft')
+			as ubisoft_hours
+from shifts
+where start_date_est 
+	< date_trunc('week', current_date)
+group by 1,2
