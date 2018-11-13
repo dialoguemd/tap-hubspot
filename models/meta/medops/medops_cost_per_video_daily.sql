cf86ed0f00cb56309d8ae379c8d13a248c492522
Jacob <38863038+jacobfrackson@users.noreply.github.com>
2018-11-13 10:46:25 -0500

    Feat: cost per video (#34)
    * Qnaire completion stats
    
    * Cost per video daily
    
    * Fix time frame for wiw
    
    * add test
    
    * Add test
    
    * Add to test
    
    * Add location name
    
    * Simplify naming
    
    * Remove day as per decree

---
+++ models/meta/medops/medops_cost_per_video_daily.sql
@@ -0,0 +1,30 @@
+with
+	wiw_shifts as (
+		select * from {{ ref('wiw_shifts') }}
+	)
+
+	, video_started as (
+		select * from {{ ref('video_started') }}
+	)
+
+	, daily_costs as (
+		select start_date_est as date
+			, sum(cost) as cost
+		from wiw_shifts
+		where position_name in ('GP', 'Nurse Practitioner')
+			and location_name = 'Virtual Care Platform'
+			and start_date_est
+				< date_trunc('week', current_date)
+		group by 1
+	)
+
+select daily_costs.date
+	, daily_costs.cost
+	, count(video_started.episode_id) as count_videos
+	, daily_costs.cost
+		/ count(video_started.episode_id)::float as cost_per_video
+from daily_costs
+inner join video_started
+	on daily_costs.date = date_trunc('day', video_started.timestamp)
+	and main_specialization in ('Family Physician', 'Nurse Practitioner')
+group by 1,2
