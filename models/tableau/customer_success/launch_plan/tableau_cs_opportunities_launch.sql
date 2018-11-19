ab1975ccc793ba28b8790d33c546cb17b1675aff
Matthieu Di Mercurio <matthieu.dimercurio@gmail.com>
2018-11-19 18:04:08 -0500

    add opps in decide stage to the dashboard (#52)

--- models/tableau/customer_success/launch_plan/tableau_cs_opportunities_launch.sql
+++ models/tableau/customer_success/launch_plan/tableau_cs_opportunities_launch.sql
@@ -1,4 +1,4 @@
 select *
 from {{ ref('salesforce_opportunities_detailed') }}
-where is_won
+where (is_won or stage_name = 'Decide')
 	and launch_date > date_trunc('week', current_date) - interval '2 weeks'
