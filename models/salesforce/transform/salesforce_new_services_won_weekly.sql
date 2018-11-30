bce638b39ea1a64342088349232d135a3c97d2ee
Matthieu Di Mercurio <matthieu.dimercurio@gmail.com>
2018-11-30 11:42:28 -0500

    fix salesforce tests (#92)
    - Not null on a sum coalesced to 0
    - New product added by the team

--- models/salesforce/transform/salesforce_new_services_won_weekly.sql
+++ models/salesforce/transform/salesforce_new_services_won_weekly.sql
@@ -5,10 +5,16 @@ with
 
 select date_trunc('quarter', close_date) as date_quarter
 	, date_trunc('week', close_date) as date_week
-	, sum(subtotal) filter(
+	, coalesce(
+		sum(subtotal) filter(
 			where product_name = 'Stress Management & Well-Being'
-		) as mrr_stress_mgmt
-	, sum(subtotal) filter(where product_name = '24/7') as mrr_24_7
+		)
+		, 0
+	) as mrr_stress_mgmt
+	, coalesce(
+		sum(subtotal) filter(where product_name = '24/7')
+		, 0
+	) as mrr_24_7
 from products
 where is_won
 group by 1,2
