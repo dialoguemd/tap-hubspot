b1e452815f430d6b4e65052995537a2f43d403a7
Jacob <38863038+jacobfrackson@users.noreply.github.com>
2018-11-30 11:00:16 -0500

    Switch from org name to org id because of duplicate names (#94)

--- models/meta/medops/medops_video_rate_by_org_monthly.sql
+++ models/meta/medops/medops_video_rate_by_org_monthly.sql
@@ -22,6 +22,7 @@ with videos as (
 
 select paid_employees_monthly.date_month
     , paid_employees_monthly.organization_name
+    , paid_employees_monthly.organization_id
     , paid_employees_monthly.account_name
     , coalesce(paid_employees_monthly.count_paid_employees, 0)
         as count_paid_employees
@@ -36,9 +37,10 @@ select paid_employees_monthly.date_month
     , case
         when count_paid_employees <> 0
         then coalesce(videos.count_videos, 0)::float / count_paid_employees
+        else 0
     end as video_rate
 from paid_employees_monthly
-left join videos using (date_month, organization_name)
+left join videos using (date_month, organization_id)
 left join ubi_consults
     on paid_employees_monthly.date_month = ubi_consults.date_month
     and paid_employees_monthly.organization_name
