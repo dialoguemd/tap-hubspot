d54ffbb1cc287f0dbd8a1b91635be8f29a85651d
Jacob <38863038+jacobfrackson@users.noreply.github.com>
2018-12-14 13:16:58 -0500

    DM-214: Fix the duplicate post issue (#113)
    * Fix the duplicate post issue
    
    * Incremental model
    
    * Update to max instead of min

--- models/messaging/transform/messaging_posts_all_time.sql
+++ models/messaging/transform/messaging_posts_all_time.sql
@@ -1,8 +1,22 @@
 
-{{ config(materialized='table') }}
+{{
+  config({
+    "materialized":"incremental",
+    "sql_where":"created_at > (select max(created_at) from {{this}})",
+    "post-hook": [
+       "DROP INDEX IF EXISTS {{ this.schema }}.index_posts_all_time_created",
+       "CREATE INDEX IF NOT EXISTS index_posts_all_time_created ON {{ this }}(created_at)"
+    ]
+  })
+}}
 
 with messaging_posts as (
-       select * from {{ ref('messaging_posts') }}
+      select * from {{ ref('messaging_posts') }}
+      -- Only add incremental jinja to messaging_posts because mm_posts is
+      -- now deprecated as of 2018-09-01
+      {% if adapter.already_exists(this.schema, this.table) and not flags.FULL_REFRESH %}
+         where created_at > (select max(created_at) from {{ this }})
+      {% endif %}
    )
 
    , test_users as (
@@ -23,7 +37,23 @@ with messaging_posts as (
       from mm_posts
    )
 
-select *
+select post_id
+    , post_type
+    , user_id
+    , mm_user_id
+    , user_type
+    , episode_id
+    , message_length
+    , count_question_marks
+    , next_appointment
+    , is_question
+    , is_internal_post
+    , mention
+    -- Take the max to take the later post and ignore Segment's manipulation
+    -- of timestamps; take the later to not interfere also with the incremental
+    -- materialization of the model
+    , max(created_at) as created_at
 from unioned
 left join test_users using (user_id)
 where test_users.user_id is null
+group by 1,2,3,4,5,6,7,8,9,10,11,12
