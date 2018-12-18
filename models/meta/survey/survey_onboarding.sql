6aac4fc52ea2b59ca4dece6359fcd2326968df68
Jacob <38863038+jacobfrackson@users.noreply.github.com>
2018-12-18 09:25:07 -0500

    Update post-onboarding survey to make Specialist Name available (#118)
    * Update survey-onboarding
    
    * Add schema file

--- models/meta/survey/survey_onboarding.sql
+++ models/meta/survey/survey_onboarding.sql
@@ -7,13 +7,24 @@ with
 		select * from {{ ref('organizations') }}
 	)
 
+	, specialists as (
+	    select choices_answer[1] as specialist_name
+	        , response_id
+	    from responses
+	    where question_id in ('DggGJwmci4M6', 'HVzuTJLk2v7d')
+	)
+
 select responses.*
+	, specialists.specialist_name
 	, organizations.organization_name
 	, organizations.account_id
 	, organizations.account_name
+	, organizations.billing_start_date as launch_date
 from responses
 left join organizations
 	using (organization_id)
+left join specialists
+	using (response_id)
 where
 -- exclude identifying questions
 -- These questions don't include any useful information
