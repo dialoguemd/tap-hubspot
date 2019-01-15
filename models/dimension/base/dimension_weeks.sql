c5968c8aca0c90e10bc9f4a6554fee5010d1f921
Jacob <38863038+jacobfrackson@users.noreply.github.com>
2019-01-15 15:56:38 -0500

    Update MTTR calc (#152)

---
+++ models/dimension/base/dimension_weeks.sql
@@ -0,0 +1,5 @@
+select generate_series(
+'2016-01-04', -- the first monday of 2016
+current_date,
+interval '1 week')
+as date_week
--- models/meta/product/product_mean_time_to_resolve.sql
+++ models/meta/product/product_mean_time_to_resolve.sql
@@ -6,17 +6,35 @@ with jira_issues as (
         select * from {{ ref('dimension_working_minutes') }}
     )
 
-select jira_issues.created_at
-    , jira_issues.issue_id
-    , jira_issues.issue_type
-    , jira_issues.resolved_at
-    , count(working_minutes.minute) as time_to_resolve_working_min
-    , count(working_minutes.minute) / 480 as time_to_resolve_working_days
-from jira_issues
-left join working_minutes 
-    on working_minutes.minute between jira_issues.created_at
-    	and coalesce(jira_issues.resolved_at, current_date)
-where jira_issues.issue_type in ('P1 Bug', 'P2 Bug')
-    and jira_issues.resolved_at
-    	< date_trunc('week', current_date) - interval '1 day'
-group by 1,2,3,4
+    , weeks as (
+        select * from {{ ref('dimension_weeks') }}
+    )
+
+    , issues as (
+        select jira_issues.issue_id
+            , jira_issues.issue_type
+            , jira_issues.resolved_at
+            , date_trunc('week', resolved_at) as date_week
+            , count(working_minutes.minute) as time_to_resolve_working_min
+            , count(working_minutes.minute) / 480 as time_to_resolve_working_days
+        from jira_issues
+        left join working_minutes
+            on working_minutes.minute between jira_issues.created_at
+                and coalesce(jira_issues.resolved_at, current_date)
+        where jira_issues.issue_type in ('P1 Bug', 'P2 Bug')
+            and jira_issues.resolved_at
+                < date_trunc('week', current_date) - interval '1 day'
+        group by 1,2,3
+    )
+
+select weeks.date_week
+    , count(issues.issue_id) filter (where issue_type = 'P1 Bug') as p1_bugs_count
+    , count(issues.issue_id) filter (where issue_type = 'P2 Bug') as p2_bugs_count
+    , sum(time_to_resolve_working_min) filter (where issue_type  = 'P1 Bug') as p1_ttr_working_min
+    , sum(time_to_resolve_working_min) filter (where issue_type  = 'P2 Bug') as p2_ttr_working_min
+    , sum(time_to_resolve_working_days) filter (where issue_type  = 'P1 Bug') as p1_ttr_working_day
+    , sum(time_to_resolve_working_days) filter (where issue_type  = 'P2 Bug') as p2_ttr_working_day
+from weeks
+left join issues
+    using (date_week)
+group by 1
