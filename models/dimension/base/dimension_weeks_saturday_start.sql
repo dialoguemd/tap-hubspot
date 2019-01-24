d0ffe0097218bfd97369ecbc00ea3443a9a57bfa
Jacob <38863038+jacobfrackson@users.noreply.github.com>
2019-01-24 17:15:31 -0500

    Redefine week to start on Saturday (#167)
    * Redefine week to start on Saturday
    
    * requested changes

--- models/dimension/base/dimension_weeks_saturday_start.sql
+++ models/dimension/base/dimension_weeks_saturday_start.sql
@@ -1,5 +1,5 @@
 select generate_series(
-'2016-01-04', -- the first monday of 2016
+'2016-01-02', -- the first saturday of 2016
 current_date,
 interval '1 week')
 as date_week
--- models/meta/product/product_mean_time_to_resolve.sql
+++ models/meta/product/product_mean_time_to_resolve.sql
@@ -7,16 +7,24 @@ with jira_issues as (
     )
 
     , weeks as (
-        select * from {{ ref('dimension_weeks') }}
+        select * from {{ ref('dimension_weeks_saturday_start') }}
     )
 
     , issues as (
         select jira_issues.issue_id
             , jira_issues.issue_type
             , jira_issues.resolved_at
-            , date_trunc('week', resolved_at) as date_week
-            , count(working_minutes.date_minute) as time_to_resolve_working_min
-            , count(working_minutes.date_minute) / 480 as time_to_resolve_working_days
+            , case
+                when extract ('dow' from jira_issues.resolved_at) > 5
+                    then date_trunc('day', jira_issues.resolved_at)
+                        - (extract ('dow' from jira_issues.resolved_at) -6)
+                        * interval '1 days'
+                    else date_trunc('day', jira_issues.resolved_at)
+                        - (extract ('dow' from jira_issues.resolved_at) +1)
+                        * interval '1 days'
+                    end as date_week
+            , count(working_minutes.minute) as time_to_resolve_working_min
+            , count(working_minutes.minute) / 480 as time_to_resolve_working_days
         from jira_issues
         left join working_minutes
             on working_minutes.date_minute between jira_issues.created_at
@@ -28,12 +36,18 @@ with jira_issues as (
     )
 
 select weeks.date_week
-    , count(issues.issue_id) filter (where issue_type = 'P1 Bug') as p1_bugs_count
-    , count(issues.issue_id) filter (where issue_type = 'P2 Bug') as p2_bugs_count
-    , sum(time_to_resolve_working_min) filter (where issue_type  = 'P1 Bug') as p1_ttr_working_min
-    , sum(time_to_resolve_working_min) filter (where issue_type  = 'P2 Bug') as p2_ttr_working_min
-    , sum(time_to_resolve_working_days) filter (where issue_type  = 'P1 Bug') as p1_ttr_working_day
-    , sum(time_to_resolve_working_days) filter (where issue_type  = 'P2 Bug') as p2_ttr_working_day
+    , count(issues.issue_id) filter (where issue_type = 'P1 Bug')
+        as p1_bugs_count
+    , count(issues.issue_id) filter (where issue_type = 'P2 Bug')
+        as p2_bugs_count
+    , sum(time_to_resolve_working_min) filter (where issue_type  = 'P1 Bug')
+        as p1_ttr_working_min
+    , sum(time_to_resolve_working_min) filter (where issue_type  = 'P2 Bug')
+        as p2_ttr_working_min
+    , sum(time_to_resolve_working_days) filter (where issue_type  = 'P1 Bug')
+        as p1_ttr_working_day
+    , sum(time_to_resolve_working_days) filter (where issue_type  = 'P2 Bug')
+        as p2_ttr_working_day
 from weeks
 left join issues
     using (date_week)
