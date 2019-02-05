41944c41739025ffdbe4d2a8ae364a90d4062c24
lp1992 <25254389+lp1992@users.noreply.github.com>
2019-02-05 09:01:08 -0500

    resolved matt comments

--- models/meta/dxa/dxa_cc_classifier_data_validated_labels.sql
+++ models/meta/dxa/dxa_cc_classifier_data_validated_labels.sql
@@ -1,9 +1,10 @@
 with validated_data as (select * from {{ ref('data_dxa_cc_classifier_data_validated') }}
-)
+    )
 
-, ccs as (select * from {{ ref('dimension_dxa_chief_complaints') }}
-)
+    , ccs as (
+        select * from {{ ref('dimension_dxa_chief_complaints') }}
+    )
 
 select * from validated_data
 left join ccs
-on validated_data.doctor_label = ccs.chief_complaint
+    on validated_data.doctor_label = ccs.chief_complaint
