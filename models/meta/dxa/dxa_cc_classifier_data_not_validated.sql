ca472455025630da1fd315b33424418898af49c6
lp1992 <25254389+lp1992@users.noreply.github.com>
2019-02-05 18:10:52 -0500

    add language to dxa cc classifier model

--- models/meta/dxa/dxa_cc_classifier_data_not_validated.sql
+++ models/meta/dxa/dxa_cc_classifier_data_not_validated.sql
@@ -10,16 +10,23 @@ with reply as (
         select * from {{ ref('data_dxa_cc_classifier_data_validated') }}
     )
 
+    , users as (
+    	select * from {{ ref('scribe_users') }}
+    )
+
 select reply.episode_id
     , episodes_chief_complaint.cc_code as shift_manager_label
     , min(reply.reply_value) as descript
     , min(reply.replied_at) as replied_at
+    , min(users.language) as lang
 from reply
 left join episodes_chief_complaint
     using (episode_id)
 left join doctor_validated
 	on reply.reply_value = doctor_validated.descript
-where reply.question_name = 'symptoms' 
+left join users
+	using (user_id)
+where reply.question_name = 'symptoms'
     and reply.reply_value is not null
     and qnaire_name in ('feeling_sick', 'chronic', 'ask_symptoms')
     and doctor_validated.doctor_label is null
