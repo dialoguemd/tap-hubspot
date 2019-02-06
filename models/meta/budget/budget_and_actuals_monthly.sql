7afb17a2a494d6ebf012d1aa3e96cae2d067926b
Jacob <38863038+jacobfrackson@users.noreply.github.com>
2019-02-06 17:50:23 -0500

    feat: budget vs actuals (DM-218) (#181)
    Create new system for analyzing budget and actuals by department
    using Metabase. New permissions system also implemented on Metabase
    
    DM-218

---
+++ models/meta/budget/budget_and_actuals_monthly.sql
@@ -0,0 +1,39 @@
+with
+	expenses as (
+		select * from {{ ref('xero_expenses_detailed') }}
+	)
+
+	, budget as (
+		select * from {{ ref('xero_budget_monthly_detailed')}}
+	)
+
+	, expenses_monthly as (
+		select date_month
+			, account_budget_category
+			, account_group
+			, sum(line_amount) as amount_actual
+		from expenses
+		group by 1,2,3
+	)
+
+select expenses_monthly.date_month
+	, expenses_monthly.account_budget_category
+	, expenses_monthly.account_group
+	, round(
+		coalesce(budget.amount, 0)::numeric
+		,2) as amount_budget
+	, round(
+		coalesce(expenses_monthly.amount_actual, 0)::numeric
+		,2) as amount_actual
+	, round(
+		(
+			coalesce(budget.amount, 0)
+			- coalesce(expenses_monthly.amount_actual, 0)
+		) :: numeric
+		,2) as delta
+from expenses_monthly
+left join budget
+	using (date_month, account_budget_category)
+-- Budget started being tracked in Oct 2018
+where expenses_monthly.date_month >= '2018-10-01'
+	and expenses_monthly.account_group <> 'N/A'
---
+++ models/meta/budget/budget_and_actuals_monthly_marketing.sql
@@ -0,0 +1,7 @@
+with
+	budget_and_actuals as (
+		select * from {{ ref('budget_and_actuals_monthly') }}
+	)
+
+select * from budget_and_actuals
+where account_group = 'Marketing Expenses'
