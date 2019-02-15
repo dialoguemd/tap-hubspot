3539c784189b0565a1ebbddd63b7c1ced85311e6
Matthieu Di Mercurio <matthieu.dimercurio@gmail.com>
2019-02-15 17:35:30 -0500

    update overhead cost (#207)

--- models/wiw/transform/wiw_costs_weekly.sql
+++ models/wiw/transform/wiw_costs_weekly.sql
@@ -1,3 +1,4 @@
+{% set overhead = 1.25 %}
 with
 	costs_daily as (
 		select * from {{ ref('wiw_costs_by_position_daily') }}
@@ -8,7 +9,7 @@ select date_trunc('week', start_date) as start_week
 		sum(virtual_costs)
 			filter(where position_name in ('GP', 'Nurse Practitioner'))
 		, 0
-	) + 1.18 * coalesce(
+	) + {{ overhead }} * coalesce(
 		sum(virtual_costs)
 			filter(where position_name not in ('GP', 'Nurse Practitioner'))
 		, 0
@@ -19,7 +20,7 @@ select date_trunc('week', start_date) as start_week
 			filter(where position_name in ('GP', 'Nurse Practitioner'))
 		, 0
 	) as virtual_video_costs_fl
-	, 1.18 * coalesce(
+	, {{ overhead }} * coalesce(
 		sum(virtual_costs)
 			filter(where position_name not in ('GP', 'Nurse Practitioner'))
 		, 0
@@ -29,16 +30,16 @@ select date_trunc('week', start_date) as start_week
 			filter(where position_name = 'GP')
 		, 0
 	) as virtual_costs_gp_fl
-	, 1.18 * coalesce(sum(virtual_costs)
+	, {{ overhead }} * coalesce(sum(virtual_costs)
 			filter(where position_name = 'Nurse Practitioner' )
 		, 0
 	) as virtual_costs_np_fl
-	, 1.18 * coalesce(sum(virtual_costs)
+	, {{ overhead }} * coalesce(sum(virtual_costs)
 			filter(where position_name in ('Nurse', 'Triage',
 				'Night Shift Nurse', 'Evening Shift Nurse'))
 		, 0
 	) as virtual_costs_nc_fl
-	, 1.18 * coalesce(sum(virtual_costs)
+	, {{ overhead }} * coalesce(sum(virtual_costs)
 			filter(where position_name not in (
 				'Nurse', 'Triage', 'Night Shift Nurse',
 				'Evening Shift Nurse', 'Nurse Practitioner', 'GP'))
