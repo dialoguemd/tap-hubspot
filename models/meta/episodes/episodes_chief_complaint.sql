b09867b17bfee10c8e5b6e5c93305541c9e0de1f
Matthieu Di Mercurio <matthieu.dimercurio@gmail.com>
2019-02-26 12:13:46 -0500

    fix: take first CC instead of last (#231)

--- models/meta/episodes/episodes_chief_complaint.sql
+++ models/meta/episodes/episodes_chief_complaint.sql
@@ -13,8 +13,8 @@ with
 			, timestamp
 			, labels.description_en as cc_label_en
 			, labels.chief_complaint is not null as is_valid_cc
-			, row_number() over (partition by episode_id order by timestamp desc)
-				as rank_desc
+			, row_number() over (partition by episode_id order by timestamp)
+				as rank
 		from dxa_cc
 		-- Inner join to exclude CCs that are not in our dimension model
 		inner join labels
@@ -28,6 +28,6 @@ select episode_id
 	, timestamp
 	, timezone('America/Montreal', timestamp) as timestamp_est
 from cc
--- Take the last CC because it is assumed this is the corrected choice
+-- Take the first CC because it is assumed this is the corrected choice
 -- Very few episodes (0.5% as verified on Feb 22, 2019) have multiple valid CCs anyways
-where rank_desc = 1
+where rank = 1
