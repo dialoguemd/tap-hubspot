b1e85d15d15ab3bc567bc86e70ac6a1e13896c76
jacob.frackson <j@frackson.com>
2019-03-19 15:01:16 -0400

    add tests and sub-bugs

--- models/meta/product/product_bugs.sql
+++ models/meta/product/product_bugs.sql
@@ -14,8 +14,7 @@ with jira_issues as (
             , created_at
             , resolved_at as closed_at
         from jira_issues
-        where issue_type in ('P2 Bug', 'P1 Bug', 'P3 Bug')
-        -- to update with sub-bug? To confirm
+        where issue_type in ('P2 Bug', 'P1 Bug', 'P3 Bug', 'Sub-bug')
             and project_name = 'Dialogue Product'
 
         union all
--- models/meta/product/product_find_rate.sql
+++ models/meta/product/product_find_rate.sql
@@ -2,15 +2,12 @@ with issues as (
         select * from {{ ref( 'jira_issues' ) }}
     )
 
-select date_trunc('week', created_at) as date_week
+select date_trunc('day', created_at) as date_day
 	, count(*) filter (where is_bug) as bugs_found_count
 	, count(*) filter (where is_bug and discovered_by = 'Tech')
 		as bugs_found_by_tech_count
 	, count(*) filter (where is_bug and discovered_by = 'Users')
 		as bugs_found_by_users_count
-	, count(*) filter (where is_bug and discovered_by = 'Tech') * 1.0
-		/ count(*) filter (where is_bug)
-		as find_rate
 from issues
 where is_bug
 group by 1
