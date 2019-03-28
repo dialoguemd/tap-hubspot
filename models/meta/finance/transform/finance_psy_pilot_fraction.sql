b70a263300d829ebc156d91a77b33f76c59579b1
Jacob <38863038+jacobfrackson@users.noreply.github.com>
2019-03-28 13:30:49 -0400

    fix: div by zero errors on two models

--- models/meta/finance/transform/finance_psy_pilot_fraction.sql
+++ models/meta/finance/transform/finance_psy_pilot_fraction.sql
@@ -7,15 +7,19 @@ with costs as (
 	)
 
 select date_trunc('month', costs.date_day) as date_month
-	, sum(costs.cc_cost) filter(where episodes.issue_type = 'psy-pilot') *1.0
-		/ sum(costs.cc_cost) as cc_fraction
-	, sum(costs.nc_cost) filter(where episodes.issue_type = 'psy-pilot') *1.0
-		/ sum(costs.nc_cost) as nc_fraction
-	, sum(costs.np_cost) filter(where episodes.issue_type = 'psy-pilot') *1.0
-		/ sum(costs.np_cost) as np_fraction
-	, (sum(costs.gp_psy_cost) filter(where episodes.issue_type = 'psy-pilot')
-		+ sum(costs.gp_other_cost) filter(where episodes.issue_type = 'psy-pilot')) *1.0
-		/ (sum(costs.gp_psy_cost) + sum(costs.gp_other_cost)) as gp_fraction
+	{% for cost_type in ['cc', 'nc', 'np'] %}
+	, case when sum(costs.{{cost_type}}_cost) > 0
+		then sum(costs.{{cost_type}}_cost) filter(where episodes.issue_type = 'psy-pilot') *1.0
+			/ sum(costs.{{cost_type}}_cost)
+		else 0
+		end as {{cost_type}}_fraction
+	{% endfor %}
+	, case when (sum(costs.gp_psy_cost) + sum(costs.gp_other_cost)) > 0
+		then (sum(costs.gp_psy_cost) filter(where episodes.issue_type = 'psy-pilot')
+			+ sum(costs.gp_other_cost) filter(where episodes.issue_type = 'psy-pilot')) *1.0
+			/ (sum(costs.gp_psy_cost) + sum(costs.gp_other_cost))
+		else 0
+		end as gp_fraction
 from costs
 left join episodes
     using (episode_id)
--- models/meta/mental_health/mental_health_phq9_user_summaries.sql
+++ models/meta/mental_health/mental_health_phq9_user_summaries.sql
@@ -40,8 +40,11 @@ select user_id
 	{% for n in ["1", "3", "6"] %}
 	, score_month{{n}}
 	, score_month{{n}} is not null as has_score_month{{n}}
-	, round(
-		(score_first - score_month{{n}})*-1.0 / score_first
-		, 2) as delta_month{{n}}
+	, case when score_first > 0
+		then round(
+			(score_first - score_month{{n}})*-1.0 / score_first
+		, 2)
+		else 0
+		end as delta_month{{n}}
 	{% endfor %}
 from summaries
