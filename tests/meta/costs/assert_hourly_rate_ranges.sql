bbe14471c03a467567040248ea3f23e2f61deb3c
Matthieu Di Mercurio <matthieu.dimercurio@gmail.com>
2019-04-11 17:16:40 -0400

    fix: test failures from cost update (#314)

--- tests/meta/costs/assert_hourly_rate_ranges.sql
+++ tests/meta/costs/assert_hourly_rate_ranges.sql
@@ -7,8 +7,8 @@ select *
 from costs_hourly_by_spec_monthly
 where date_month > '2018-06-01'
 	and (
-		cc_hourly not between 35 and 60
-		or cc_hourly_ops not between 50 and 75
-		or nc_hourly not between 60 and 105
-		or nc_hourly_ops not between 80 and 135
+		cc_hourly not between 35 and 65
+		or cc_hourly_ops not between 50 and 80
+		or nc_hourly not between 60 and 110
+		or nc_hourly_ops not between 80 and 150
 	)
