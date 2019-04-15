7ef0a10643585f07e6019de7fbb4ebf19c194f37
Matthieu Di Mercurio <matthieu.dimercurio@gmail.com>
2019-04-15 15:18:49 -0400

    fix: test to validate hourly range (#317)
    * fix: test to validate hourly range
    
    Use only recent data since the process and cost allocation changed a lot over the past year
    
    * fix typo

--- tests/meta/costs/assert_hourly_rate_ranges.sql
+++ tests/meta/costs/assert_hourly_rate_ranges.sql
@@ -5,10 +5,11 @@ with
 
 select *
 from costs_hourly_by_spec_monthly
-where date_month > '2018-06-01'
+-- Calibrated in April 2019
+where date_month >= '2019-02-01'
 	and (
-		cc_hourly not between 35 and 65
-		or cc_hourly_ops not between 50 and 80
-		or nc_hourly not between 60 and 110
-		or nc_hourly_ops not between 80 and 150
+		cc_hourly not between 35 and 45
+		or cc_hourly_ops not between 50 and 60
+		or nc_hourly not between 65 and 80
+		or nc_hourly_ops not between 85 and 105
 	)
