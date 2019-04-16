859f9afd87d9fbcc819de5ba9bd03c6530e6e72d
Matthieu Di Mercurio <matthieu.dimercurio@gmail.com>
2019-04-16 09:54:32 -0400

    fix: column name conflict (#327)

--- models/meta/medops/transform/medops_shifts.sql
+++ models/meta/medops/transform/medops_shifts.sql
@@ -7,7 +7,20 @@ with
 		select * from {{ ref('practitioners') }}
 	)
 
-select *
+select {{
+	dbt_utils.star(
+		from=ref('wiw_shifts_detailed'),
+		relation_alias='wiw_shifts',
+	) }}
+
+	, {{
+		dbt_utils.star(
+			from=ref('practitioners'),
+			except=['email', 'user_id', 'first_name', 'last_name'],
+			relation_alias='practitioners'
+		)
+	}}
+
 from wiw_shifts
 left join practitioners
 	using (user_id)
