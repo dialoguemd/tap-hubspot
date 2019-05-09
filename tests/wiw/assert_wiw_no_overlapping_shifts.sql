77b4c31873e6c7ad5f5f05dc509ecb69e9fe16d1
Jacob <38863038+jacobfrackson@users.noreply.github.com>
2019-05-09 14:50:41 -0400

    fix(scribe): add new feature to test

--- tests/wiw/assert_wiw_no_overlapping_shifts.sql
+++ tests/wiw/assert_wiw_no_overlapping_shifts.sql
@@ -1,8 +1,8 @@
 with
 	wiw_shifts as (
         select * from {{ ref('wiw_shifts') }}
-        # Don't test future shifts because they're more likely to be
-        # overlapping or for some other reason not finalized
+        -- Don't test future shifts because they're more likely to be
+        -- overlapping or for some other reason not finalized
         where start_time < current_date
     )
 
