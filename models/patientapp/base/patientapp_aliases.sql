0535b6c10dc120db4295e5b70621137b58d284ae
Jacob <38863038+jacobfrackson@users.noreply.github.com>
2019-05-27 09:14:24 -0400

    fix(patientapp-aliases): rank instead of group (#397)

--- models/patientapp/base/patientapp_aliases.sql
+++ models/patientapp/base/patientapp_aliases.sql
@@ -1,5 +1,14 @@
-select previous_id as anonymous_id
-	, user_id
-from patientapp.aliases
-where user_id is not null
-group by 1,2
+with
+	ranked as (
+		select previous_id as anonymous_id
+			, user_id
+			, row_number()
+				over (partition by previous_id order by timestamp)
+				as rank
+		from patientapp.aliases
+		where user_id is not null
+	)
+
+select *
+from ranked
+where rank = 1
