0b2caeb261e870f5e4ac569ea4fe1a31122b16b8
Jacob <38863038+jacobfrackson@users.noreply.github.com>
2019-05-28 12:00:11 -0400

    fix(cp_activity): recalibrate and redefine test

--- tests/meta/cp_activity/assert_cp_activity_is_active_rate.sql
+++ tests/meta/cp_activity/assert_cp_activity_is_active_rate.sql
@@ -10,20 +10,23 @@ with
 			, sum(time_spent) filter (where
 					is_active and episode_id is not null
 				) / sum(time_spent) filter(where is_active) :: float
-			as fraction_time_spent_in_episode
-			, sum(time_spent) filter (where
-					is_active and shift_location = 'Virtual Care Platform'
-				) / sum(time_spent) filter(where
-					shift_location = 'Virtual Care Platform'
-				) :: float as fraction_active_platform_time_spent
+			    as fraction_time_spent_in_episode
+			, sum(time_spent) filter (where is_active)
+			    / sum(time_spent) :: float
+			    as fraction_active_platform_time_spent
 		from cp_activity
 		where date > '2018-10-01'
+			and shift_position
+				not in ('Night Shift Nurse', 'CC Tech', 'Medical Archivist')
+			and main_specialization
+				in ('Care Coordinator', 'Nurse Clinician')
+			and shift_location = 'Virtual Care Platform'
 		group by 1
 	)
 
 select *
 from weeks
--- Calibrated in April 2019
-where fraction_activities < 0.95
-	or fraction_time_spent_in_episode < 0.83
+-- Calibrated in May 2019
+where fraction_activities < 0.96
+	or fraction_time_spent_in_episode < 0.80
 	or fraction_active_platform_time_spent < 0.66
